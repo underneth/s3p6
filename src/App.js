367260a2da19ac3ef1b7a48c844c8320114df540
David Oliveira <davidoliveira@MacBook-Pro-de-David.local>
2025-03-25 18:41:31 +0000

    cart app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+
+function App() {
+    return (
+    <Layout>    
+      <Cart />
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
+
+
